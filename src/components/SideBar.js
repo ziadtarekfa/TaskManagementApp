519b4835fdd09b0adbcd84bc6189786cd1df6f00
Ziad Tarek <ziadtarekfa@gmail.com>
2022-10-24 22:02:45 +0200

    retrieved boards and displayed them in sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,38 @@
+import { getDocs, collection } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../database';
 import '../componentsStyles/SideBar.css';
 const SideBar = () => {
+
+    const [boardsList, setBoardsList] = useState([]);
+    const [boardsCount, setCount] = useState();
+    useEffect(() => {
+        const boardsRef = collection(db, "Boards");
+        getDocs(boardsRef).then((boards) => {
+            let count = 0;
+            const tempBoardsList = [];
+            boards.forEach((board) => {
+                tempBoardsList.push(board.data());
+                count = count + 1;
+
+            });
+            setCount(count);
+            setBoardsList(tempBoardsList);
+        });
+
+    }, []);
     return (
         <div className="sidebar">
             <h1>kanban</h1>
-            <p>ALL BOARDS (8)</p>
+            <p>{`ALL BOARDS (${boardsCount || ""})`}</p>
             <div className='boards'>
-                <button>Platform Launch</button>
-                <button>Platform Launch</button>
-                <button>Platform Launch</button>
+                {
+                    boardsList.map((board) => {
+                        return (
+                            <button>{board.name}</button>
+                        );
+                    })
+                }
             </div>
         </div>
     );
