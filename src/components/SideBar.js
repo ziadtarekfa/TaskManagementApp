2f11bc7a6096afde2918ff650f2803328e7c42ed
Ziad Tarek <ziadtarekfa@gmail.com>
2022-10-25 18:32:57 +0200

    implemented viewBoard tasks feature

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,37 @@
 import { getDocs, collection } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { db } from '../database';
 import '../componentsStyles/SideBar.css';
+import { setDoingTasks, setDoneTasks, setToDoTasks } from '../redux/tasksSlice';
 const SideBar = () => {
 
+
+    // const { toDoTasks, doneTasks, doingTasks } = useSelector((state) => state.tasks);
+    const dispatch = useDispatch();
+    function viewBoard(event) {
+        const collectionRef = collection(db, "Boards", event.currentTarget.id, "Tasks");
+        const toDotemp = [];
+        const doneTemp = [];
+        const doingTemp = [];
+        getDocs(collectionRef).then((data) => {
+
+            data.forEach((doc) => {
+                if (doc.data().status === "TODO") {
+                    toDotemp.push(doc.data());
+                }
+                else if (doc.data().status === "DONE") {
+                    doneTemp.push(doc.data());
+                }
+                else if (doc.data().status === "DOING") {
+                    doingTemp.push(doc.data());
+                }
+            });
+            dispatch(setToDoTasks(toDotemp));
+            dispatch(setDoneTasks(doneTemp));
+            dispatch(setDoingTasks(doingTemp));
+        });
+    }
     const [boardsList, setBoardsList] = useState([]);
     const [boardsCount, setCount] = useState();
     useEffect(() => {
@@ -12,13 +40,13 @@ const SideBar = () => {
             let count = 0;
             const tempBoardsList = [];
             boards.forEach((board) => {
-                tempBoardsList.push(board.data());
+                tempBoardsList.push(board);
                 count = count + 1;
-
             });
             setCount(count);
             setBoardsList(tempBoardsList);
         });
+
 
     }, []);
     return (
@@ -29,7 +57,7 @@ const SideBar = () => {
                 {
                     boardsList.map((board) => {
                         return (
-                            <button>{board.name}</button>
+                            <button onClick={viewBoard} key={board.id} id={board.id}>{board.data().name}</button>
                         );
                     })
                 }
